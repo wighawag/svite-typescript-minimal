e1b52404e842660567fe0cdfe0c6d1785de104cf
Wighawag <wighawag@gmail.com>
2020-08-20 12:16:06 +0100

    use rollup-plugin-node-builtins

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,9 @@
-const dependenciesRequiringNodeBuiltins = [
-    "@ethersproject/random",
-    "bn.js",
-    "@ethersproject/web",
-    "svite-typescript-minimal", // why ?
-    "@ethersproject/providers",
-    "ws",
-    "brorand",
-    "inherits"
-]
-module.exports = {
-    optimizeDeps: {
-        allowNodeBuiltins: {
-            includes: (name) => dependenciesRequiringNodeBuiltins.indexOf(name) !== -1
-        }
-    },
+import builtins from 'rollup-plugin-node-builtins';
 
-    /*
-[vite] Dep optimization failed with error:
-[vite]: Rollup failed to resolve import "utf-8-validate" from "node_modules\@ethersproject\providers\node_modules\ws\lib\validation.js".
-This is most likely unintended because it can break your application at runtime.
-If you do want to externalize this module explicitly add it to
-`rollupInputOptions.external`
-    */
+module.exports = {
     rollupInputOptions: {
-        external: ["utf-8-validate", "bufferutil"]
+        plugins: [
+            builtins({crypto: true})
+        ]
     }
   }
