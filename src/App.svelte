571ade1d401880cbe60e7e66af0f31dbcc2a594b
Wighawag <wighawag@gmail.com>
2020-08-20 07:41:38 +0100

    add component

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,14 @@
 <script lang="ts">
+  import Button from "./components/Button.svelte";
   const world = 'world'; // edit world and save to see hmr update
 </script>
 
 <style>
   h1 {
-    color: orangered; /* change color an save to see hmr update */
+    color: red; /* change color an save to see hmr update */
   }
 </style>
 
-<h1>Hello {world}</h1>
-<p>Open App.svelte in your editor and change something to see HMR in action</p>
+<h1>Hello d {world}</h1>
+<p>Open Appd sds.svelte in your editor and change something to see HMR in action</p>
+<Button/>
