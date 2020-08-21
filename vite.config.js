40b5cc9b4a9639b59acbb31b421682bde8341ef0
Wighawag <wighawag@gmail.com>
2020-08-21 22:33:27 +0100

    full

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-const dependenciesRequiringNodeBuiltins = ["bn.js"]; // TODO check if it works
-module.exports = {
-    optimizeDeps: {
-        allowNodeBuiltins: {
-            includes: (name) => dependenciesRequiringNodeBuiltins.indexOf(name) !== -1
-        }
-    },
-};
